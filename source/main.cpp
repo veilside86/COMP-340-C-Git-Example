c9dbfee32ec27faa8a815399f33aeb9b09344c0d
Christian Esteves <c.esteves92@gmail.com>
2020-01-23 14:37:08 -0500

    Clarified instructions.

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,13 @@
  * Assignment: 
  * Create a new branch titled '{first_name}'s_function'.
  * Create a new class titled with your first name. e.g. 'Christian.cpp' and 'Christian.hpp'
- * In the .hpp, create a private static const string, and a public void function 'displayName()'
- * In the .cpp, assign the value to the string and write the body of the function.
+ * In the .hpp, include the String class and create the following variable: 'private static const std::string name;'
+ * Also in the .hpp, create a public void function 'displayName()'
+ * In the .cpp, assign a string literal containing your first name to the 'name' variable, and write the body of the 'displayName()' function.
  * Modify 'main.cpp' to include your .hpp file, and invoke the 'displayName()' function.
  * Push & commit this change to your branch.
  */
 
-int 
+int main(){
+    
 }
