cb8db79066cb94207d1d2be43d342714adf2b1b6
Christian Esteves <c.esteves92@gmail.com>
2020-01-23 14:27:08 -0500

    Example pushed.

---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+#include "Christian.hpp"
+/* 
+ * Assignment: 
+ * Create a new branch titled '{first_name}_function'.
+ * Create a new class titled with your first name. e.g. 'Christian.cpp' and 'Christian.hpp'
+ * In the .hpp, create a private static const string, and a public void function 'displayName()'
+ * In the .cpp, assign the value to the string and write the body of the function.
+ * Modify 'main.cpp' to include your .hpp file, and invoke the 'displayName()' function.
+ * Push & commit this change to your branch.
+ */
+
+int main(){
+	Christian::displayName();
+}
