2adf761c74740b1e659f0ac4e6f6489969cec443
Junseok Hur <gjwnstjr100@gmail.com>
2020-02-13 20:38:07 -0500

    Class completed

---
+++ source/Junseok.cpp
@@ -0,0 +1,7 @@
+#include "Junseok.hpp"
+
+const std::string Junseok::name = "Junseok";
+
+void Christian::displayName(){
+    std::cout << "Hello, " << Junseok::name << "!\n";
+}
---
+++ source/Junseok.hpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+class Junseok{
+private:
+    static const std::string name;
+
+public:
+    static void displayName();
+};
